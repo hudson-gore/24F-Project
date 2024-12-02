2e2c73adce930a1ab75ce0c2791028dce6f98529
hudson-gore <gore.h@northeastern.edu>
2024-12-02 08:49:20 -0500

    basic contact get route

--- api/backend/contacts/contact_routes.py
+++ api/backend/contacts/contact_routes.py
@@ -12,3 +12,23 @@ contacts = Blueprint('contacts', __name__)
 
 # Routes
 
+# Get all of the contacts from the system
+@contacts.route('/contacts/<type>', methods=['GET'])
+def get_contacts(type):
+    # Define allowed table names 
+    allowed_tables = {'employees, students'}
+
+    # Check if request is valid
+    if type not in allowed_tables:
+        return make_response(jsonify({"error": "Invalid type specified"}))
+    
+    cursor = db.get_db().cursor()
+
+    query = ''' SELECT * FROM {type}}'''
+    cursor.execute(query)
+
+    theData = cursor.fetchall()
+
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
