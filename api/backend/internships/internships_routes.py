c8b92e8d27d8ca51ad56751eafd074328d7e0c4c
hudson-gore <gore.h@northeastern.edu>
2024-12-02 19:46:25 -0500

    get route for studnet contacts for specific internships

--- api/backend/internships/internships_routes.py
+++ api/backend/internships/internships_routes.py
@@ -12,3 +12,20 @@ from backend.ml_models.model01 import predict
 internships = Blueprint('internships', __name__)
 
 # Routes
+
+# Returns all of the students that have held a specific internship
+# in the past and their contact info
+@internships.route('/internships/<position>', methods=['GET'])
+def internship_holders(position):
+    cursor = db.get_db().cursor()
+
+    query = '''SELECT s.FirstName, s.LastName, s.Year, s.Major, s.Email, s.Phone
+               FROM students s
+               JOIN internships i ON s.StudentID = i.PositionHolder
+               WHERE i.PositionID = %s'''
+    
+    cursor.execute(query, (position,))
+    theData = cursor.fetchall()
+    the_response = make_response(jsonify(theData))
+    the_response.status = 200
+    return the_response
