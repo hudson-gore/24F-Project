358142f8cf4a0d3863912c460ea1a2a536fbcab4
hudson-gore <gore.h@northeastern.edu>
2024-12-02 19:37:10 -0500

    creating internships routes blueprint + folder

---
+++ api/backend/internships/internships_routes.py
@@ -0,0 +1,14 @@
+# Import statements
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from flask import abort
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
+# Creating new blueprint object
+internships = Blueprint('internships', __name__)
+
+# Routes
