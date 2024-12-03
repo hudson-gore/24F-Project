a0bc8b700860fa3e2a185ba1a8e516cb73c27f09
hudson-gore <gore.h@northeastern.edu>
2024-12-03 11:25:23 -0500

    companies get route

--- api/companies/companies_routes.py
+++ api/companies/companies_routes.py
@@ -12,3 +12,19 @@ from backend.ml_models.model01 import predict
 companies = Blueprint('companies', __name__)
 
 # Routes
+
+# Return the company profile of a specific company
+@companies.route('/companies/<id>', methods=['GET'])
+def get_comp_prof(id):
+    cursor = db.get_db().cursor()
+
+    query = '''SELECT *
+               FROM companies
+               WHERE CompanyID = %s'''
+    cursor.execute(query,(id,))
+
+    theData = cursor.fetchall()
+
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
