a72f28803e57e24af8ab041556c054d637ef37fe
hudson-gore <gore.h@northeastern.edu>
2024-12-03 12:40:12 -0500

    profile catchall get route

--- api/backend/profile/profile_routes.py
+++ api/backend/profile/profile_routes.py
@@ -13,6 +13,32 @@ profile = Blueprint('profile', __name__)
 
 # Routes
 
+# Return all of the student or employee profiles
+@profile.route('/profile/<type>', methods=['GET'])
+def get_all_profiles(type):
+    
+    if type not in ['student', 'employee']:
+        return "Invalid contact type. Use 'student' or 'employee'."
+    
+    queries = {
+        'student': '''SELECT *
+                      FROM students
+                    ''',
+        'employee': '''SELECT *
+                       FROM employees
+                    '''
+    }
+
+    query = queries[type]
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+
+    return the_response
+
+
 # Return the profile for a specific user
 @profile.route('/profile/<type>/<id>', methods=['GET'])
 def find_profile(type, id):
