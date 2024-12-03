e25cc3ea4a6707b609f532c407066daf4302b197
hudson-gore <gore.h@northeastern.edu>
2024-12-02 21:02:43 -0500

    profile post route

--- api/backend/profile/profile_routes.py
+++ api/backend/profile/profile_routes.py
@@ -39,3 +39,72 @@ def find_profile(type, id):
     the_response.status_code = 200
 
     return the_response
+
+# Add a profile to the database
+@profile.route('/profile/<type>', methods=['POST'])
+def add_profile(type):
+    
+    if type not in ['student', 'employee']:
+        abort(400, description="Invalid contact type. Use 'student' or 'employee'.")
+
+    profile_data = request.json
+
+    if type == 'student':
+        required_fields = ['FirstName', 'LastName', 'Major', 'ExpectedGrad', 'Year', 
+                           'ProfileDetails', 'Phone', 'Email', 'ProfileManager']
+        if not all(field in profile_data for field in required_fields):
+            return "Missing required fields in request data."
+        
+        first = profile_data['FirstName']
+        last = profile_data['LastName']
+        major = profile_data['Major']
+        grad = profile_data['ExpectedGrad']
+        year = profile_data['Year']
+        bio = profile_data['ProfileDetails']
+        phone = profile_data['Phone']
+        email = profile_data['Email']
+        prof_manager = profile_data['ProfileManager']
+
+        data = (first, last, major, grad, year, bio, phone, email, prof_manager)
+        
+    elif type == 'employee':
+        required_fields = ['FirstName', 'LastName', 'JobTitle', 'ProfileDetails', 'Phone', 
+                           'Email', 'ContactManager', 'ProfileManager', 'Company']
+        if not all(field in profile_data for field in required_fields):
+            return "Missing required fields in request data."
+        
+        first = profile_data['FirstName']
+        last = profile_data['LastName']
+        title = profile_data['JobTilte']
+        bio = profile_data['ProfileDetails']
+        phone = profile_data['Phone']
+        email = profile_data['Email']
+        con_manager = profile_data['ContactManager']
+        prof_manager = profile_data['ProfileManager']
+        company = profile_data['Company']
+
+        data = (first, last, title, bio, phone, email, con_manager, prof_manager, company)
+
+    queries = {
+        'student': '''INSERT INTO students (FirstName, LastName, Major, ExpectedGrad, Year, 
+                                             ProfileDetails, Phone, Email, ProfileManager)
+                      VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ''',
+        'employee': '''INSERT INTO employees (FirstName, LastName, JobTile, ProfileDetails, Phone,
+                                              Email, ContactManager, ProfileManager, Company)
+                       VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    '''
+    }
+
+    query = queries[type]
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+
+    response = make_response('Successfully added profile!')
+    response.status_code = 200
+    return response
+    
+        
+    
