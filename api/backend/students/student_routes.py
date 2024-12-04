b634061863d98474cf83a5f81937f65a4023341d
mhi11 <maxhill2022@gmail.com>
2024-12-04 15:37:28 -0500

    added student get route

---
+++ api/backend/students/student_routes.py
@@ -0,0 +1,35 @@
+# Import statements
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from flask import abort
+from backend.db_connection import db
+from backend.students import students_data
+
+# new Blueprint object
+students = Blueprint('students', __name__)
+
+# routes
+
+# return all students
+@students.route('/students', methods=['GET'])
+def get_all_students():
+
+    current_app.logger.info()
+    cursor = db.get_db().cursor()
+
+    query = '''SELECT s.FirstName, s.LastName, s.Major, s.Email
+               FROM students AS s
+               ORDER BY s.LastName'''
+    
+    cursor.execute(query)
+    theData = cursor.fetchall()
+
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# returns student(s) with given name
