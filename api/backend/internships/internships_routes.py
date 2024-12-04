17bea66f8134f5248199e1b8eabddbe3e8207f08
bhoju12 <155978371+bhoju12@users.noreply.github.com>
2024-12-04 16:13:59 -0500

    delete internship get route added back

--- api/backend/internships/internships_routes.py
+++ api/backend/internships/internships_routes.py
@@ -105,5 +105,17 @@ def update_internship():
     cursor = db.get_db().cursor()
     cursor.execute(query, data)
     db.get_db().commit()
-
+    
     return "Contact info updated successfully!"
+
+# Delete a specific internship role from the system
+@internships.route('/internships/<position>', methods=['DELETE'])
+def delete_internship(position):
+
+    query = '''DELETE FROM internships WHERE PositionID = %s'''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "Internship deleted successfully!"
